207cd51e5d785630af47887f3969c4861e5ff583
iki1uc <entertrainer@web.de>
2026-07-06 00:02:15 +0200

    Update axiRoom.js

--- core/axiRoom.js
+++ core/axiRoom.js
@@ -1,1 +1,8 @@
+function axiRoom(input){
+    if(input.includes("1tel")) return "1TEL";
+    if(input.includes("2tel")) return "2TEL";
+    if(input.includes("3tel")) return "3TEL";
+    if(input.includes("4tel")) return "4TEL";
+    return "UNKNOWN_ROOM";
+}
 

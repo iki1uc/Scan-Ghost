85dafeda4c2cbdf8caf216e3c219f7f2af64c7f2
iki1uc <entertrainer@web.de>
2026-07-06 01:18:56 +0200

    Update autoBuild.js

--- autoBuild.js
+++ autoBuild.js
@@ -1,1 +1,17 @@
 
+export async function autoBuild(missingList) {
+
+    for (const entry of missingList) {
+
+        const file = entry.split(": ")[1];
+
+        const emptyContent = "{}";
+
+        await fetch(file, {
+            method: "PUT",
+            body: emptyContent
+        });
+    }
+
+    return "Auto‑Build abgeschlossen.";
+}

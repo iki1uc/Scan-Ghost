c46a348254f239362e2f5328016e87a1f6493186
iki1uc <entertrainer@web.de>
2026-07-06 00:31:20 +0200

    Update scanghost.js

--- scanghost.js
+++ scanghost.js
@@ -1,1 +1,18 @@
+async function runScanGhost(input){
+    const axiRoom = (await loadModule('./core/axiRoom.js')).default;
+    const auraDirection = (await loadModule('./core/auraDirection.js')).default;
+    const on3Depth = (await loadModule('./core/on3Depth.js')).default;
+    const axisShift = (await loadModule('./core/axisShift.js')).default;
+    const mnmmNorm = (await loadModule('./core/mnmmNorm.js')).default;
+
+    const interpret = (await loadModule('./core/interpret.js')).default;
+
+    return interpret(input, {
+        axiRoom,
+        auraDirection,
+        on3Depth,
+        axisShift,
+        mnmmNorm
+    });
+}
 

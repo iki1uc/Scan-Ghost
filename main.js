28598037fa31f3208ca12695d2824f5d277cf529
iki1uc <entertrainer@web.de>
2026-07-06 01:24:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { respoBewertung } from "./bewertung.js";
+import { moveKalkuel } from "./move.js";
 
-document.getElementById("bewertung").onclick = () => {
+document.getElementById("move").onclick = () => {
     const out = document.getElementById("output");
-    out.style.color = "#00ccff";
+    out.style.color = "#ffaa00";
     out.style.animation = "pulse 1.8s infinite";
-    out.textContent = respoBewertung();
+    out.textContent = moveKalkuel();
 };

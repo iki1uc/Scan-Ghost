12ee138209c93b20c636985ac17466afe60930a6
iki1uc <entertrainer@web.de>
2026-07-06 00:03:25 +0200

    Add mnmmNorm function to classify MNMM inputs

--- core/mnmmNorm.js
+++ core/mnmmNorm.js
@@ -1,1 +1,9 @@
+function mnmmNorm(input){
+    if(input.includes("MNMM-MOVE")) return "MOVE";
+    if(input.includes("MNMM-ENGINE")) return "ENGINE";
+    if(input.includes("MNMM-ORDER")) return "ORDER";
+    if(input.includes("MNMM-MANEUVER")) return "MANEUVER";
+    if(input.includes("MNMM-ARG")) return "ARGUMENT";
+    return "UNKNOWN_MNMM";
+}
 

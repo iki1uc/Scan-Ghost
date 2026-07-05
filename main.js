5c524d6ad9410ef6ae1eeb8a9f201132c8eec337
iki1uc <entertrainer@web.de>
2026-07-06 01:16:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import { marktCheck } from "./marktCheck.js";
 
 document.getElementById("run").onclick = async () => {
+    const input = document.getElementById("input").value;
+    const output = scanGhostText(input);
+    document.getElementById("output").textContent = output;
+};
+
+document.getElementById("markt").onclick = async () => {
     const result = await marktCheck();
     document.getElementById("output").textContent = result;
 };

5885f90598eefd8402ed59328208ac814048dfe3
iki1uc <entertrainer@web.de>
2026-07-06 00:32:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+document.getElementById("run").onclick = async () => {
+    const input = document.getElementById("input").value;
+    const output = await interpretInput(input);
+    document.getElementById("output").textContent = output;
+};
 

e13a508b29aeca357c6d436a75a8adfa3aa46be4
iki1uc <entertrainer@web.de>
2026-07-06 00:02:34 +0200

    Update auraDirection.js

--- core/auraDirection.js
+++ core/auraDirection.js
@@ -1,1 +1,10 @@
+function auraDirection(input){
+    if(input.includes("AU_to_RA")) return "AU→RA";
+    if(input.includes("RA_to_AU")) return "RA→AU";
+    if(input.includes("indoor")) return "INDOOR";
+    if(input.includes("outdoor")) return "OUTDOOR";
+    if(input.includes("forward")) return "FORWARD";
+    if(input.includes("back")) return "BACK";
+    return "UNKNOWN_DIRECTION";
+}
 

00dd49ad5c0e4c696126bad5e1b718f1f57a160a
iki1uc <entertrainer@web.de>
2026-07-06 00:03:08 +0200

    Add axisShift function to identify axis types

--- core/axisShift.js
+++ core/axisShift.js
@@ -1,1 +1,8 @@
+function axisShift(input){
+    if(input.includes("AX")) return "AX";
+    if(input.includes("XA")) return "XA";
+    if(input.includes("AX_OUT")) return "AX-OUT";
+    if(input.includes("XA_IN")) return "XA-IN";
+    return "UNKNOWN_AXIS";
+}
 

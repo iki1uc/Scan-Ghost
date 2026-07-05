5d2f02447eaf3cae88cc2c13e3e29ffad01bf972
iki1uc <entertrainer@web.de>
2026-07-06 00:02:50 +0200

    Update on3Depth.js

--- core/on3Depth.js
+++ core/on3Depth.js
@@ -1,1 +1,8 @@
+function on3Depth(input){
+    if(input.includes("3x3")) return "DEPTH-3";
+    if(input.includes("3x6")) return "DEPTH-6";
+    if(input.includes("3x9")) return "DEPTH-9";
+    if(input.includes("PI-3x9")) return "DEPTH-PI9";
+    return "UNKNOWN_DEPTH";
+}
 
